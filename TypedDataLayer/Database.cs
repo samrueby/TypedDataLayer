ca759a001da7ace770d237a9b1115ea995e16bba
Sam Rueby <samrueby@gmail.com>
2019-04-26 15:42:55 -0400

    Cache the DataAccessState provided in the Database becuase reading configuration information is expensive, but it's unexpected to change.

--- TypedDataLayer/Database.cs
+++ TypedDataLayer/Database.cs
@@ -7,23 +7,25 @@ namespace TypedDataLayer {
 	/// Functions that can be used to access the database.
 	/// </summary>
 	public static class Database {
+		private static readonly DataAccessState dataAccessState = new DataAccessState();
+
 		/// <summary>
 		/// Executes a query in the default isolation level.
 		/// </summary>
 		public static void ExecuteInDbConnection( [ InstantHandle ] Action method ) =>
-			new DataAccessState().ExecuteWithThis( () => DataAccessState.Current.DatabaseConnection.ExecuteWithConnectionOpen( method ) );
+			dataAccessState.ExecuteWithThis( () => DataAccessState.Current.DatabaseConnection.ExecuteWithConnectionOpen( method ) );
 
 		/// <summary>
 		/// Executes a query in the default isolation level.
 		/// </summary>
 		public static T ExecuteInDbConnection<T>( [ InstantHandle ] Func<T> method ) =>
-			new DataAccessState().ExecuteWithThis( () => DataAccessState.Current.DatabaseConnection.ExecuteWithConnectionOpen( method ) );
+			dataAccessState.ExecuteWithThis( () => DataAccessState.Current.DatabaseConnection.ExecuteWithConnectionOpen( method ) );
 
 		/// <summary>
 		/// Executes a query in Snapshot isolation.
 		/// </summary>
 		public static void ExecuteInDbConnectionWithTransaction( [ InstantHandle ] Action method ) =>
-			new DataAccessState().ExecuteWithThis(
+			dataAccessState.ExecuteWithThis(
 				() => DataAccessState.Current.DatabaseConnection.ExecuteWithConnectionOpen(
 					() => DataAccessState.Current.DatabaseConnection.ExecuteInTransaction( method ) ) );
 
@@ -31,7 +33,7 @@ namespace TypedDataLayer {
 		/// Executes a query in Snapshot isolation.
 		/// </summary>
 		public static T ExecuteInDbConnectionWithTransaction<T>( [ InstantHandle ] Func<T> method ) =>
-			new DataAccessState().ExecuteWithThis(
+			dataAccessState.ExecuteWithThis(
 				() => DataAccessState.Current.DatabaseConnection.ExecuteWithConnectionOpen(
 					() => DataAccessState.Current.DatabaseConnection.ExecuteInTransaction( method ) ) );
 
@@ -40,7 +42,7 @@ namespace TypedDataLayer {
 		/// </summary>
 		/// <param name="method"></param>
 		public static void ExecuteInDbConnectionWithTransactionWithCaching( [ InstantHandle ] Action method ) =>
-			new DataAccessState().ExecuteWithThis(
+			dataAccessState.ExecuteWithThis(
 				() => DataAccessState.Current.DatabaseConnection.ExecuteWithConnectionOpen(
 					() => DataAccessState.Current.DatabaseConnection.ExecuteInTransaction( () => DataAccessState.Current.ExecuteWithCache( method ) ) ) );
 
@@ -48,7 +50,7 @@ namespace TypedDataLayer {
 		/// Executes a query in Snapshot isolation with caching. Not safe to use when modifying data.
 		/// </summary>
 		public static T ExecuteInDbConnectionWithTransactionCaching<T>( [ InstantHandle ] Func<T> method ) =>
-			new DataAccessState().ExecuteWithThis(
+			dataAccessState.ExecuteWithThis(
 				() => DataAccessState.Current.DatabaseConnection.ExecuteWithConnectionOpen(
 					() => DataAccessState.Current.DatabaseConnection.ExecuteInTransaction( () => DataAccessState.Current.ExecuteWithCache( method ) ) ) );
 	}
