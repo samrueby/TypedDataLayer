0cdf9a402842243b9c35d40ed55e8dae82aa6061
Greg Smalter <gsmalter@brossgroup.com>
2020-01-27 15:08:28 -0500

    Broke testing into two projects.

---
+++ TestTdlGeneration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace TestTdlGeneration {
+	class Program {
+		static void Main( string[] args ) {
+			Debug.WriteLine( "Hello World!" );
+			//var workingDirectory = ConfigurationManager.AppSettings[ "token" ];
+			CommandRunner.Program.LoadConfigAndRunUpdateAllDependentLogic( @"C:\GitHub\TypedDataLayer\TestTdlGeneration", true );
+		}
+	}
+}
