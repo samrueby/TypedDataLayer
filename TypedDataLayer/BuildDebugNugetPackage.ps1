d8aa114d2868aa54d93a42a3b05e39da997b2f35
Sam Rueby <samrueby@gmail.com>
2021-03-05 10:37:35 -0500

    Add a PowerShell script to generate debug packages.

---
+++ TypedDataLayer/BuildDebugNugetPackage.ps1
@@ -0,0 +1,4 @@
+﻿$now = Get-Date
+$version = [string]::Format("{0}.{1}.{2}.{3}$OctoVersionSuffix",$now.Year, $now.Month, $now.Day,$now.Hour*10000+$now.Minute*100+$now.Second)
+
+nuget.exe pack TypedDataLayer.nuspec -Prop Configuration=Debug -Version $version -Suffix alpha
